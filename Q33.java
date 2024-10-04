615fe8933e441d3faeed4dd80c0726d86af7a4b5
Raydaa Barak <Raydaabarak@gmail.com>
2024-10-04 05:05:34 +0300

    Update Q33.java

--- Q33.java
+++ Q33.java
@@ -1,4 +1,63 @@
-1- First aid course,2000.0
-2- Anatomy1 course,1000.0
-3- Medical principals,500.0
-4- Emergency Medicine,1000.0
+//Raydaa Almabadbi. 444002439. Group3
+
+// Java code for linearly search x in arr[]. If x
+
+// is present then return its location, otherwise
+
+// return -1
+
+class LinearSearch {
+
+            // This function returns index of element x in arr[]
+
+            static int search(int arr[], int n, int x)
+
+            {
+
+                        for (int i = 0; i < n; i++) {
+
+                                // Return the index of the element if the element
+
+                                    // is found
+
+                                    if (arr[i] == x)
+
+                                                return i;
+
+                        }
+
+ 
+
+                        // return -1 if the element is not found
+
+                        return -1;
+
+            }
+
+ 
+
+            public static void main(String[] args)
+
+            {
+
+                        int[] arr = { 3, 4, 1, 7, 5 };
+
+                        int n = arr.length;
+
+                        int x = 4;
+
+                        int index = search(arr, n, x);
+
+                        if (index == -1)
+
+                          System.out.println("Element is not present in the array");
+
+                        else
+
+                          System.out.println("Element found at position " + index);
+
+            }
+
+}
+
+//------------------------------------------------------------------------------------------------------
